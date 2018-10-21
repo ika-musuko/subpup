019e1844313303478cf980a96c8589f3347a6f9c
sherwyn <sherwyn.sen@gmail.com>
2018-10-20 19:07:22 -0700

    set up basic routes

--- backend/project/routes.py
+++ backend/project/routes.py
@@ -1,6 +1,23 @@
 from project import app
 
+@app.route("/list_dog")
+def list_dog():
+    return "list a new dog"
+
+@app.route("/about")
+def about():
+    return "about page"
+
+@app.route("/insert_name")
+def insert_name():
+    return "insert name for new users"
+
+@app.route("/log_in")
+def log_in():
+    return "login route, google login"
+
 @app.route("/")
 @app.route("/index")
 def index():
     return "dog app"
+
